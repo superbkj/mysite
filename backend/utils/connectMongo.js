dd502205007be01cb2d418079503c969a32483de
superbkj <honda1340916@gmail.com>
2023-02-20 23:15:49 +0900

    set mongoose strictQuery option false

--- backend/utils/connectMongo.js
+++ backend/utils/connectMongo.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const { info, error } = require('./logger');
 const config = require('./config');
 
+mongoose.set('strictQuery', false);
+
 const connectMongo = () => {
   mongoose.connect(config.MONGODB_URI);
   mongoose.connection.on('error', (err) => {
